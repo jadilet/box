e4e5f264c5e208f4c134f25b5e51a02967b75e37
jadilet <adilet.571@gmail.com>
2018-11-08 11:13:12 +0600

    refactored build script

--- build.sh
+++ build.sh
@@ -1,21 +1,55 @@
 #!/usr/bin/env bash
 
-PROJECT_PATH="$HOME/OptDyn/Project/packer"
+##############################################################
+#                     PARAMETER                              #
+#------------------------------------------------------------#
+# $1 => provider name                                        #
+# default values for linux: vmware-iso, virtualbox-iso, qemu #
+# default values for osx: parallels-iso                      #
+#------------------------------------------------------------#
+# $2 => git branch name                                      #
+# default value is master                                    #
+#------------------------------------------------------------#
+# $3 => vagrant box name                                     #
+# default values is stretch                                  #
+#------------------------------------------------------------#
+
+
+# default values
+PROJECT_PATH="$HOME/packer"
 BRANCH=master
 VERSION=`cat $PROJECT_PATH/version`
 OS=`uname`
-PROVIDER=""
+BOX=stretch
+GIT_REPOSITORY=https://github.com/subutai-io/packer.git
 
-if [ "$BRANCH" = "master" ]; then
-  BOX=subutai/stretch-master
+## clean
+rm -rf $PROJECT_PATH
+
+## clone packer project
+cd $HOME
+git clone $GIT_REPOSITORY
+cd $PROJECT_PATH
+
+if [ "$2" = "prod" ]; then
+  # "master" is prod branch for 
+  # vagrant subutai boxes provision script
+  BRANCH=prod
+  BOX_NAME=subutai/$BOX
+  git checkout master
+  git pull origin master
 else
-  BOX=subutai/stretch
+  # "stage" is master branch for 
+  # vagrant subutai boxes provision script
+  BOX_NAME=subutai/$BOX-master
+  git checkout stage
+  git pull origin stage
 fi
 
 if [ -n "$1" ]; then
   PROVIDERS="$1"
-  for box in $PROVIDERS; do
-    case "$box" in
+  for provider in $PROVIDERS; do
+    case "$provider" in
       "vmware-iso")
         break
         ;;
@@ -28,7 +62,7 @@ if [ -n "$1" ]; then
       "parallels-iso")
         break
         ;;
-      *) echo "Bad hypervisor name in hypervisor list: $box"; exit 1
+      *) echo "Bad hypervisor name in hypervisor list: $provider"; exit 1
         ;;
     esac
   done
@@ -41,20 +75,21 @@ elif [ -z "$PROVIDERS" ]; then
 fi
 
 
-mkdir -p $BRANCH
+mkdir -p $HOME/$BRANCH
+pkdir -p $HOME/$BRANCH/$BOX
 
 if [ ! -d "$PROJECT_PATH" ]; then
   echo "Incorrect project path: "$PROJECT_PATH
   exit 1
 fi
 
-BUILD_DIRECTORY="$BRANCH/$VERSION"
+BUILD_DIRECTORY="$HOME/$BRANCH/$BOX/$VERSION"
 
 # clean build directory if exist
-#if [ -d $BUILD_DIRECTORY ]; then
-#  echo "Removing build directory: "$BUILD_DIRECTORY
-#  rm -rf $BRANCH/$VERSION
-#fi
+if [ -d $BUILD_DIRECTORY ]; then
+  echo "Removing build directory: "$BUILD_DIRECTORY
+  rm -rf $BRANCH/$VERSION
+fi
 
 mkdir -p $BUILD_DIRECTORY
 
@@ -62,9 +97,14 @@ for hypervizor in $PROVIDERS; do
   # Go project path
   cd $PROJECT_PATH
 
-  echo 'n' | ./build.sh stretch $hypervizor $BRANCH
+  echo 'n' | ./build.sh $BOX $hypervizor $BRANCH
+
+  if [ $? -gt 0 ]; then
+    exit 1
+  fi
+
   # move box to build directory
-  mv vagrant-subutai-stretch-*.box ~/box/$BUILD_DIRECTORY
+  mv vagrant-subutai-$BOX-*.box $BUILD_DIRECTORY
   case "$hypervizor" in
       "vmware-iso")
         PROVIDER="vmware_desktop";
@@ -82,12 +122,14 @@ for hypervizor in $PROVIDERS; do
 
   mkdir -p $PROJECT_PATH/test
   cd $PROJECT_PATH/test
+  # clean 
+  rm -rf .vagrant
   rm -f Vagrantfile
   echo "-----------------"
   echo "Provider: "$PROVIDER
   echo "-----------------"
 
-  vagrant init $BOX
+  vagrant init $BOX_NAME
   SUBUTAI_ENV=$BRANCH vagrant up --provider $PROVIDER
   vagrant destroy -f
   SUBUTAI_ENV=$BRANCH DISK_SIZE=200 vagrant up --provider $PROVIDER
